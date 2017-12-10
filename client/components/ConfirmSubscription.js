833634b2ea13f1316f60c930dd22a77fb4fd50f7
Damian Nardelli <damiox@gmail.com>
2017-12-10 18:33:00 -0300

    added bootstrap and improved the react views

--- client/components/ConfirmSubscription.js
+++ client/components/ConfirmSubscription.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import {Col, Alert} from 'react-bootstrap';
 
 export class ConfirmSubscription extends React.Component {
+	// this.props.match.params.confirmid
 	render() {
-		return <div>CONFIRM subscription?? { this.props.match.params.confirmid }</div>
+		return (
+                        <Col smOffset={3} sm={6} mdOffset={4} md={4}>
+				<Alert bsStyle="success text-center">
+					<strong>Congratulations!</strong><br />You've been subscribed to this Newsletter.<br />
+					You will be receiving daily emails at 6am
+				</Alert>
+				<Alert bsStyle="danger text-center">
+					This confirmation link has expired or it is invalid. <br /> ¯\_(ツ)_/¯ <br />
+					You may want to try subscribing again <a href="/subscribe-now">now</a>.
+				</Alert>
+			</Col>
+		)
 	}
 }
 
--- client/components/SubscribeReceipt.js
+++ client/components/SubscribeReceipt.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import {Alert, Form, FormGroup, FormControl, Col, ControlLabel, Checkbox, Button} from 'react-bootstrap';
 
 export class SubscribeReceipt extends React.Component {
 	render() {
-		return <div>Hmm.. new subscription??</div>
+		return (
+			<Col smOffset={3} sm={6} mdOffset={4} md={4}>
+			  <Form horizontal>
+			    <FormGroup>
+			      <ControlLabel>Email Address</ControlLabel>
+			      <FormControl type="email" placeholder="Email" />
+			    </FormGroup>
+
+			    <FormGroup>
+			      <ControlLabel>Location</ControlLabel>
+			      <FormControl componentClass="select">
+				<option value="" selected disabled>Where do you live?</option>
+				<option value="tz1">America/NYC</option>
+				<option value="tz2">America/Seattle</option>
+			      </FormControl>
+			    </FormGroup>
+
+			    <FormGroup>
+			      <Col mdOffset={2} md={8}>
+			        <Button bsStyle="primary" bsSize="large" type="submit" block>
+			          Subscribe
+			        </Button>
+			      </Col>
+			    </FormGroup>
+			  </Form>
+
+			  <Alert bsStyle="info text-center">
+			    An e-mail has been sent to <strong>email@email.com</strong>. Please follow the instructions there to confirm this subscription.<br />
+			    You want to subscribe another email? Click <a href="/subscribe-now">Here</a>.
+			  </Alert>
+			</Col>
+		)
 	}
 }
 
